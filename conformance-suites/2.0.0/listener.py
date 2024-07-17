2b82cc94eeb2b7e57002df15e4e0fecbb50beca3
Yudong <maghsk2017@gmail.com>
2024-07-17 22:07:44 +0800

    54321

--- conformance-suites/2.0.0/listener.py
+++ conformance-suites/2.0.0/listener.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 CORS(app)  # 允许所有来源的跨域请求
 
 # 定义存储JSON文件的目录
-SAVE_DIR = 'saved_jsons'
+SAVE_DIR = 'bundle_saved_jsons'
 if not os.path.exists(SAVE_DIR):
     os.makedirs(SAVE_DIR)
 
 def get_sha256_filename(data):
     """获取JSON数据的SHA-256哈希值作为文件名，格式为SHA256.json"""
+    # data["url"] = data["url"].split("?")[0]
     json_str = json.dumps(data, sort_keys=True)  # 将JSON数据转换为字符串，并排序键以确保一致性
     sha256_hash = hashlib.sha256(json_str.encode()).hexdigest()
     return f"{sha256_hash}.json"
@@ -28,7 +29,7 @@ def save_json():
     if request.is_json:
         data = request.get_json()
         # filename = get_random_filename()
-        filename = get_sha256_filename(data)
+        filename = get_sha256_filename(data['url'])
         filepath = os.path.join(SAVE_DIR, filename)
         with open(filepath, 'w') as json_file:
             json.dump(data, json_file)
